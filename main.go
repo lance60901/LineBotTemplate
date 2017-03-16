695fe5fea99f01708d53dff66b99fbcf7a607b0a
lance60901 <lance60901@gmail.com>
2017-03-16 16:43:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -44,17 +44,17 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	/*
+	
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-					log.Print(err)
-				}
+				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+				//	log.Print(err)
+				//}
 				
 			}
 		}
 	}
-	*/
+	
 }
